cc642bd41a0d3ab5e8ebba171cf694e7d171ac1d
Alexander Mostrous <mostrousalexander@gmail.com>
2020-03-22 13:22:25 +0200

    DataHolder.java file containing dummy data changed.

--- src/utility/DataHolder.java
+++ src/utility/DataHolder.java
@@ -10,37 +10,37 @@ public abstract class DataHolder {
 	
 	public static void createDummyShowList(){
 
-		Play show1 = new Play("Hamlet");
-		show1.addShow(new Show(show1,"28/3/18", "17:30"));
-		show1.addShow(new Show(show1,"20/3/18", "18:30"));
-		show1.addShow(new Show(show1,"10/2/18", "18:30"));
-		plays.add(show1);
+		Play play1 = new Play("Hamlet");
+		play1.addShow(new Show(play1,"28/3/18", "17:30"));
+		play1.addShow(new Show(play1,"20/3/18", "18:30"));
+		play1.addShow(new Show(play1,"10/2/18", "18:30"));
+		plays.add(play1);
 		
-		Play show2 = new Play("Long Day's Journey Into Night");
-		show2.addShow(new Show(show1,"12/4/18", "17:30"));
-		show2.addShow(new Show(show1,"1/3/18", "20:00"));
-		show2.addShow(new Show(show1,"10/1/18", "21:30"));
-		plays.add(show2);
+		Play play2 = new Play("Long Day's Journey Into Night");
+		play2.addShow(new Show(play2,"12/4/18", "17:30"));
+		play2.addShow(new Show(play2,"1/3/18", "20:00"));
+		play2.addShow(new Show(play2,"10/1/18", "21:30"));
+		plays.add(play2);
 		
-		Play show3 = new Play("Who's Afraid of Virginia Woolf?");
-		show3.addShow(new Show(show1,"5/5/18", "21:30"));
-		show3.addShow(new Show(show1,"6/4/18", "17:30"));
-		show3.addShow(new Show(show1,"1/7/18", "22:00"));
-		plays.add(show3);
+		Play play3 = new Play("Who's Afraid of Virginia Woolf?");
+		play3.addShow(new Show(play3,"5/5/18", "21:30"));
+		play3.addShow(new Show(play3,"6/4/18", "17:30"));
+		play3.addShow(new Show(play3,"1/7/18", "22:00"));
+		plays.add(play3);
 		
-		Play show4 = new Play("Death of a Salesman");
-		show4.addShow(new Show(show1,"3/7/18", "20:00"));
-		show4.addShow(new Show(show1,"5/7/18", "22:00"));
-		show4.addShow(new Show(show1,"17/8/18", "19:45"));
-		plays.add(show4);
+		Play play4 = new Play("Death of a Salesman");
+		play4.addShow(new Show(play4,"3/7/18", "20:00"));
+		play4.addShow(new Show(play4,"5/7/18", "22:00"));
+		play4.addShow(new Show(play4,"17/8/18", "19:45"));
+		plays.add(play4);
 		
-		Play show5 = new Play("Oedipus Rex");
-		show5.addShow(new Show(show1,"28/8/18", "19:45"));
-		show5.addShow(new Show(show1,"14/9/18", "17:30"));
-		show5.addShow(new Show(show1,"30/9/18", "20:00"));
-		show5.addShow(new Show(show1,"17/8/18", "19:45"));
-		show5.addShow(new Show(show1,"1/11/18", "18:30"));
-		plays.add(show5);
+		Play play5 = new Play("Oedipus Rex");
+		play5.addShow(new Show(play5,"28/8/18", "19:45"));
+		play5.addShow(new Show(play5,"14/9/18", "17:30"));
+		play5.addShow(new Show(play5,"30/9/18", "20:00"));
+		play5.addShow(new Show(play5,"17/8/18", "19:45"));
+		play5.addShow(new Show(play5,"1/11/18", "18:30"));
+		plays.add(play5);
 		
 	}
 
